3bb95d964e85be4224746f0e04667a0967b1edb3
Alefh Sousa <alefh.sousa@gmail.com>
2019-04-27 09:07:58 -0300

    aula 01

---
+++ src/main/java/cotuba/cli/Main.java
@@ -0,0 +1,34 @@
+package cotuba.cli;
+
+import cotuba.application.Cotuba;
+
+import java.nio.file.Path;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		LeitorOpcoesCLI opcoesCLI = new LeitorOpcoesCLI(args);
+
+		Path diretorioDosMD = opcoesCLI.getDiretorioDosMD();
+		String formato = opcoesCLI.getFormato();
+		Path arquivoDeSaida = opcoesCLI.getArquivoDeSaida();
+		boolean modoVerboso = opcoesCLI.isModoVerboso();
+
+		try {
+
+			Cotuba cotuba = new Cotuba();
+			cotuba.executa(formato, diretorioDosMD, arquivoDeSaida);
+
+			System.out.println("Arquivo gerado com sucesso: " + arquivoDeSaida);
+
+		} catch (Exception ex) {
+			System.err.println(ex.getMessage());
+			if (modoVerboso) {
+				ex.printStackTrace();
+			}
+			System.exit(1);
+		}
+	}
+
+}
